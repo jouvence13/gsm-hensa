77f9b786f76adaec43775dd0c2d75e08341f0b62
jouvence13 <spaceweb1997.com>
2025-04-28 10:27:52 +0100

    added a simple route for experimentatin

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,11 +3,17 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "hello" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (<div>
+    <h1>Welcome to React Router!</h1>
+    <p>
+      To get started, edit <code>app/routes/home.tsx</code> and save to reload.
+    </p>
+  </div>);
+  
 }
---
+++ app/routes/test.tsx
@@ -0,0 +1,7 @@
+
+export default function Home() {
+  return (<div>
+    <h1>Page de test</h1>
+  </div>);
+  
+}
